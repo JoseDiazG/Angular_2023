33f548341b5bf5acaa517952683f466476a78dc2
Jose Luis Diaz G <josethp1@hotmail.com>
2023-11-09 18:26:50 -0500

    03-update-functions

---
+++ 01-introduction-typescript/src/topics/03-functions.ts
@@ -0,0 +1,44 @@
+function addNumber(a:number, b:number):number{
+    return a + b
+}
+
+//craete arrow function
+const addNumberArrow = (a:number, b:number):string => {
+    return `${a + b }`
+}
+
+function multiply(firtsNumber:number, secondNumber:number, base:number = 2){
+    return firtsNumber * secondNumber
+}
+
+// const result: number = addNumbers(1, 2);
+// const result2: string = addNumbersArrow(1, 2);
+// const multiplyResult: number = multiply(5);
+// console.log({ result, result2, multiplyResult })
+
+interface Character{
+    name:string,
+    hp:number,
+    showHp: () => void;
+}
+
+const healCharacter = (character:Character, amount:number) => {
+    character.hp += amount;
+}
+
+const strider: Character = {
+    name: 'Strider',
+    hp: 50,
+    showHp() {
+        console.log(`Puntos de vida ${ this.hp }`);
+    }
+}
+
+
+healCharacter( strider, 10 );
+healCharacter( strider, 50 );
+
+
+strider.showHp();
+
+export {};
