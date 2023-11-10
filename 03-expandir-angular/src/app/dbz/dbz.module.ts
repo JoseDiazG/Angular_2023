e68110a991bbe944c48dead1c3126305f03c17f2
JoseDG <josethp1@gmail.com>
2023-11-10 11:58:55 -0500

    Creando componente dbz list - add-character

--- 03-expandir-angular/src/app/dbz/dbz.module.ts
+++ 03-expandir-angular/src/app/dbz/dbz.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 
 
 import { MainPageComponent } from './pages/main-page.component';
+import { ListComponent } from './components/list/list.component';
+import { AddCharacterComponent } from './components/add-character/add-character.component';
 
 
 
 @NgModule({
   declarations: [
-    MainPageComponent
+    MainPageComponent,
+    ListComponent,
+    AddCharacterComponent
   ],
   imports: [
     CommonModule
