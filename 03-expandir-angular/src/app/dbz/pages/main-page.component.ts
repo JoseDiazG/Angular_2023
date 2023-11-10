2299eef40fdf6168a27ef230e83b95ba99bdde8e
JoseDG <josethp1@gmail.com>
2023-11-10 12:44:12 -0500

    utilizando el @input

--- 03-expandir-angular/src/app/dbz/pages/main-page.component.ts
+++ 03-expandir-angular/src/app/dbz/pages/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Character } from '../interfaces/Character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
 })
 
 export class MainPageComponent{
+
+  public characters: Character[] = [{
+    name: 'Krillin',
+    power: 100
+  },{
+    name: 'Goku',
+    power: 950
+  },{
+    name: 'Vegueta',
+    power: 52300
+  }];
   
 }
