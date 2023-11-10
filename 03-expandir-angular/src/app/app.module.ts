e68ad2d716c79fab33f1eaa85d3eddd46e80919d
JoseDG <josethp1@gmail.com>
2023-11-10 11:03:03 -0500

    finalizando el component main-page

--- 03-expandir-angular/src/app/app.module.ts
+++ 03-expandir-angular/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//importacion de modulos
+import { DbzModule } from './dbz/dbz.module';
+
+//importacion de componentes
 import { AppComponent } from './app.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DbzModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
