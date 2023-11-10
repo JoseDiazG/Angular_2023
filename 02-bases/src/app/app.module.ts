fa0834761ff8e834de2991e7dbedb826d4b0f4cf
JoseDG <josethp1@gmail.com>
2023-11-10 09:13:43 -0500

    Counter Finish

--- 02-bases/src/app/app.module.ts
+++ 02-bases/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CounterModule } from './counter/components/counter.module';
+
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CounterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
