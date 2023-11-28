63e3ef1f3709b53ac8d549d0673ecb47a3522a11
Jose Luis Diaz G <josethp1@hotmail.com>
2023-11-28 13:19:02 -0500

    add navbar

--- 09-AppPipes/src/app/shared/components/menu/menu.component.ts
+++ 09-AppPipes/src/app/shared/components/menu/menu.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'shared-menu',
   templateUrl: './menu.component.html',
 })
 export class MenuComponent {
+
+  public menuItems:MenuItem[] = [];
+
+  ngOnit() {
+    this.menuItems = [
+      {
+        label: 'Pipes de Angular',
+        icon: 'pi pi-desktop',
+        items: [
+          {
+            label: 'Textos y Fechas'
+          }
+        ]
+      }
+    ]
+  }
 
 }
