2c149c411580eac4dca6b8b70651fbd612b91a0d
Jose Luis Diaz G <josethp1@hotmail.com>
2023-12-01 09:05:49 -0500

    add login and register

--- 11-heroesApp/src/app/app.module.ts
+++ 11-heroesApp/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -13,6 +14,8 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+
     AppRoutingModule,
     SharedModule,
   ],
