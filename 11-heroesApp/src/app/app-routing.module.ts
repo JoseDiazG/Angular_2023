f025e7db2f22f8f852c645595345b83da1fbce78
Jose Luis Diaz G <josethp1@hotmail.com>
2023-12-06 12:40:56 -0500

    Proteccion de Rutas con Guards

--- 11-heroesApp/src/app/app-routing.module.ts
+++ 11-heroesApp/src/app/app-routing.module.ts
@@ -2,15 +2,24 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 
-//dominio.com
+import { PublicGuard } from './auth/guards/public.guard';
+import { AuthGuard } from './auth/guards/auth.guard';
+
+
+
+// dominio.com/
 const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule ),
+    canActivate: [ PublicGuard ],
+    canMatch: [ PublicGuard ]
   },
   {
     path: 'heroes',
     loadChildren: () => import('./heroes/heroes.module').then( m => m.HeroesModule ),
+    canActivate: [ AuthGuard ],
+    canMatch: [ AuthGuard ]
   },
   {
     path: '404',
@@ -26,7 +35,6 @@ const routes: Routes = [
     redirectTo: '404',
   }
 ];
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- 11-heroesApp/src/app/auth/pages/login-page/login-page.component.ts
+++ 11-heroesApp/src/app/auth/pages/login-page/login-page.component.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html'
 })
 export class LoginPageComponent {
+
+  constructor(
+    private AuthService: AuthService,
+    private router:Router
+  ){}
+
+  onLogin(): void {
+
+    this.AuthService.login('fernando@gmail.com','123456')
+      .subscribe( user => {
+
+        this.router.navigate(['/']);
+
+      });
+
+  }
 
 }
