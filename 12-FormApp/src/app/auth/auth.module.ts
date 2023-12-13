d894398b6cddb6cd13101244e1c53a19459956cb
Jose Luis Diaz G <josethp1@hotmail.com>
2023-12-13 16:00:20 -0500

    add module Forms

--- 12-FormApp/src/app/auth/auth.module.ts
+++ 12-FormApp/src/app/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
@@ -12,7 +13,8 @@ import { AuthRoutingModule } from './auth-routing.module';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
