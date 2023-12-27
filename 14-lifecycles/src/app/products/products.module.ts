a8884bf98026dcc3374ac776d8c1c7ee5fb1c5e2
Jose Luis Diaz G <josethp1@hotmail.com>
2023-12-27 08:49:08 -0500

    finish lifecycles angular

---
+++ 14-lifecycles/src/app/products/products.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+import { ProductComponent } from './pages/product/product.component';
+import { PriceComponent } from './components/price/price.component';
+import { ProductsRoutingModule } from './products-routing.module';
+
+
+
+@NgModule({
+  declarations: [
+    ProductComponent,
+    PriceComponent,
+  ],
+  imports: [
+    CommonModule,
+    ProductsRoutingModule
+  ]
+})
+export class ProductsModule { }
